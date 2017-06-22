aa4d31272fe3752815c1e8a310a90a509bc3ee75
Taneli Otala <taneli@otala.com>
2017-06-21 22:11:41 -0700

    Cleanup

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,24 +1,21 @@
-
 import java.sql.Timestamp
 import slick.lifted.Tag
 import slick.jdbc.H2Profile.api._
+import sys.process._
 
 
 /**
   * Created by totala on 6/13/17.
   */
 
-//type Timestamp = sql.Timestamp
-
 class Attacks(tag: Tag) extends Table[(Int, Timestamp, Int, Int, Int, Int, String)](tag, "ATTACKS") {
   def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-  def ts = column[Timestamp]("TS")
+  def ts = column[Timestamp]("TS")    // When incident occurred
   def sip = column[Int]("SIP")        // SourceIP
   def dip = column[Int]("DIP")        // DestinationIP
   def dport = column[Int]("DPORT")    // Destination Port
   def typ = column[Int]("TYPE")       // Type of incident/attack, ToDo: Create Enum
   def desc = column[String]("TXT")    // Syslog line of incident
-  // Every table needs a * projection with the same type as the table's type parameter
   def * = (id, ts, sip, dip, dport, typ, desc)
 }
 
@@ -39,19 +36,34 @@ class Whois(tag: Tag) extends Table[(Int, Timestamp, Timestamp, String)](tag, "W
   def who = column[String]("WHO")
   def * = (ip, start, stop, who)
 }
-//abstract class TableName(val tableName: String)
 
-object main {
+/*
+ External programs:
+  - nmap
+  - traceroute
+  - [j]whois
+  - host (or could use DNS resolver libs)
+  * The programs need to exist, or otherwise this node can't be a scanner node
+  * If 'which' does not exist, then progs are called w/out absolute path...
+  * ...later it might be goodto add some type of anti-trojan verification of the progs
+ */
 
-  object loader extends App {
-    print("args: ")
-    args foreach print _
-    println()
+object main extends App {
 
-    val db = Database.forURL("jdbc:h2:mem:test1;DB_CLOSE_DELAY=-1", driver="org.h2.Driver")
-    val sess = db.createSession()
-  }
+  print("args: ")
+  args foreach print _
+  println()
+
+  val db = Database.forURL("jdbc:h2:mem:test1;DB_CLOSE_DELAY=-1", driver="org.h2.Driver")
+  val sess = db.createSession()
+
+  val nmap = findprog("nmap")
+  val traceroute = findprog("traceroute")
+  val whois = findprog("whois")
+
+  val r = Seq("nmap", "192.168.254.5").!!
 
 
+  def findprog(prog: String): String = Seq("which", prog).!!.trim
 
 }
