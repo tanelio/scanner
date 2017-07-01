8f87acb44bd842bc34d8591e32e94d4e2d464a34
tanelio <taneli@otala.com>
2017-07-01 09:31:35 -0700

    SyslogReceiver #2

--- src/main/scala/SyslogReceiver.scala
+++ src/main/scala/SyslogReceiver.scala
@@ -78,3 +78,22 @@ class SyslogReceiver(val port: Int)  extends Actor {
   }
 
 }
+
+class Listener(nextActor: ActorRef) extends Actor {
+  import context.system
+  IO(Udp) ! Udp.Bind(self, new InetSocketAddress("localhost", 0))
+
+  def receive = {
+    case Udp.Bound(local) =>
+      context.become(ready(sender()))
+  }
+
+  def ready(socket: ActorRef): Receive = {
+    case Udp.Received(data, remote) =>
+      val processed = // parse data etc., e.g. using PipelineStage
+        socket ! Udp.Send(data, remote) // example server echoes back
+      nextActor ! processed
+    case Udp.Unbind  => socket ! Udp.Unbind
+    case Udp.Unbound => context.stop(self)
+  }
+}
