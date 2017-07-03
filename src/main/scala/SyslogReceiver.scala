de58a9f7e397a7add2eeac1c3cd439e7dbd86035
tanelio <taneli@otala.com>
2017-07-02 17:59:06 -0700

    SyslogReceiver; cleanup

--- src/main/scala/SyslogReceiver.scala
+++ src/main/scala/SyslogReceiver.scala
@@ -27,35 +27,15 @@ class SyslogReceiver extends Actor {
 
 object SyslogReceiver {
   val logger = LoggerFactory.getLogger(classOf[SyslogReceiver])
-  SyslogReceiver(Syslog)
 }
 
 /*
 class SyslogReceiver(val port: Int)  extends Actor {
 
-  import SyslogReceiver._
-
-  private val inetSockAddress: InetSocketAddress = new InetSocketAddress("0.0.0.0", port)
   private val encoding = "US-ASCII"
 
-  logger.debug(s"SyslogReceiver: listening on UDP port $port")
-
-  def receive = {
-    case StartStream =>
-      try {
         IO(Udp) ! Udp.Bind(self, inetSockAddress, List(new ReuseAddress()))
         logger.debug(s"SyslogReceiver: Started")
-      } catch {
-        case e: Exception => logger.error("StartStream error", e)
-      }
-    case Udp.Bound(local: InetSocketAddress) =>
-      try {
-        logger.debug(s"SyslogReceiver: UDP bound on port $port}")
-        context.become(ready(sender))
-      } catch {
-        case e: Exception => logger.error("UDP bind error", e)
-      }
-  }
 
   private def ready(connection: ActorRef): Receive = {
     case Udp.Received(d: ByteString, f: InetSocketAddress) =>
@@ -65,35 +45,31 @@ class SyslogReceiver(val port: Int)  extends Actor {
       } catch {
         case t: Exception => logger.error(s"Error in UDP Received: ${t.getMessage}")
       }
-
-    case Udp.Unbind =>
-      logger.debug(s"SyslogReceiver: Unbind")
-      connection ! Udp.Unbind
-
-    case Udp.Unbound =>
-      logger.debug(s"SyslogReceiver: Unbound")
-      context.stop(self)
   }
-
 }
 */
 
 class SyslogReceiver(nextActor: ActorRef) extends Actor {
   import context.system
+  import SyslogReceiver._
   IO(Udp) ! Udp.Bind(self, new InetSocketAddress("0.0.0.0", 1514))
 
   def receive = {
     case Udp.Bound(local) =>
+      logger.info(s"SyslogReceiver: bound $local")
       context.become(ready(sender()))
   }
 
   def ready(socket: ActorRef): Receive = {
     case Udp.Received(data, remote) =>
-      val processed = // parse data etc., e.g. using PipelineStage
-        socket ! Udp.Send(data, remote) // example server echoes back
-      nextActor ! processed
-    case Udp.Unbind  => socket ! Udp.Unbind
-    case Udp.Unbound => context.stop(self)
+      logger.debug(s"SyslogReceiver/Received: $data")
+      nextActor ! data
+    case Udp.Unbind  =>
+      logger.info(s"SyslogReceiver/Unbind")
+      socket ! Udp.Unbind
+    case Udp.Unbound =>
+      logger.info(s"SyslogReceiver/Unbound")
+      context.stop(self)
   }
 }
 
