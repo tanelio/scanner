7a629e0f3f5f0ad14d8ce0a888e47faddf2be162
tanelio <taneli@otala.com>
2017-07-10 13:53:20 -0400

    Rules/ruler

--- src/main/scala/Ruler.scala
+++ src/main/scala/Ruler.scala
@@ -2,9 +2,14 @@
   * Created by totala on 7/9/17.
   */
 package ruler {
+  import main.main.{db, rules}
 
   object Ruler {
-
+    // initialize rules from db
+    db.run(rules.result).map(_.foreach {
+      case (pattern, reps, findtime) =>
+        println(pattern, reps, findtime)
+    })
   }
 
 }
