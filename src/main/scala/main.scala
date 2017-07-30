7c895aa2d8d5fff016c87f0ffb1c469584347eb0
tanelio <taneli@otala.com>
2017-07-30 14:07:32 -0400

    Rules, insert attacks

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -49,8 +49,9 @@ package main {
     def * = (ip, start, stop, who)
   }
 
-  class Rules(tag: Tag) extends Table[(Int, String, Int, Int, Int, Boolean)](tag, "RULES") {
+  class Rules(tag: Tag) extends Table[(Int, String, String, Int, Int, Int, Boolean)](tag, "RULES") {
     def id = column[Int]("ID", O.PrimaryKey)
+    def preamble = column[String]("PREAMBLE")
     def pattern = column[String]("PATTERN")
     def reps = column[Int]("REPS")
     def findtime = column[Int]("FINDTIME")
@@ -60,7 +61,7 @@ package main {
     // tcp/udp
     // ignoreip
     // target matching
-    def * = (id, pattern, reps, findtime, bantime, active)
+    def * = (id, preamble, pattern, reps, findtime, bantime, active)
   }
 
   /*
@@ -100,11 +101,11 @@ package main {
       (attacks.schema ++ scans.schema ++ whois.schema ++ rules.schema).create,
 
       // Jul 17 21:21:19 srv2v sshd[11066]: Received disconnect from 116.31.116.37: 11:  [preauth]
-      rules += (1, "^sshd.*Received disconnect from $ipv4: .*\\[preauth\\]", 1, 0, 3600, true),
+      rules += (1, "", "^sshd.*Received disconnect from $ipv4: .*\\[preauth\\]", 1, 0, 3600, true),
       // Jul 17 21:30:10 srv2v dovecot: pop3-login: Disconnected (auth failed, 1 attempts): user=<device@dr-kalai.com>, method=PLAIN, rip=158.69.103.43, lip=162.206.51.1
-      rules += (2, "^dovecot: pop3-login: Disconnected.*rip=$ipv4, lip=", 2, 10, 3600, true),
+      rules += (2, "", "^dovecot: pop3-login: Disconnected.*rip=$ipv4, lip=", 2, 10, 3600, true),
       // Jul 17 21:41:07 srv2v sm-mta[11778]: v6I4f33V011778: mail.actus-ilw.co.uk [92.42.121.202] (may be forged) did not issue MAIL/EXPN/VRFY/ETRN during connection to MTA
-      rules += (3, "^sm-mta\\[.*\\[$ipv4\\].* did not issue MAIL/EXPN/VRFY/ETRN during connection to MTA", 1, 0, 3600, true)
+      rules += (3, "", "^sm-mta\\[.*\\[$ipv4\\].* did not issue MAIL/EXPN/VRFY/ETRN during connection to MTA", 1, 0, 3600, true)
     )
 
     Await.result(db.run(setup), 30 seconds)
