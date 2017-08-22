3c020326d86adc66d7c14ac1c24fdcdfac333c28
tanelio <taneli@otala.com>
2017-08-22 18:17:06 -0400

    Actions, WIP

--- src/main/scala/Action.scala
+++ src/main/scala/Action.scala
@@ -1,19 +1,14 @@
-import akka.actor.Actor
-import slick.jdbc.H2Profile.api._
 import akka.actor.{Actor, Props}
-import main.main._
-
+import slick.jdbc.H2Profile.api._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by totala on 8/22/17.
   */
-case class act(ip: String)
-case class unact(ip: String)
-
 package action {
 
   import akka.actor.ActorRef
-  import main.main.{db, actions}
+  import main.main._
 
   import scala.collection.mutable
 
@@ -22,16 +17,19 @@ package action {
     db.run(actions.result).map(_.foreach {
       case (id, action) =>
         actionmap += (id -> system.actorOf(Props[Action], action))
-      )}
-
+    })
   }
+
+  case class act(ip: String)
+  case class unact(ip: String)
 
   class Action(val action: String) extends Actor {
     def receive = {
-      case act =>
-      case unact =>
+      case act(ip) =>
+        println(s"act = $ip")
+      case unact(ip) =>
+        println(s"unact = $ip")
     }
-
   }
 
 }
